f3a43027e60bbfb474d8dee99568a17d4b872687
stellacosta038 <139595986+stellacosta038@users.noreply.github.com>
2025-01-25 00:22:11 +0000

    projeto

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ app.use('/api/products',productRoutes);
 //Inicializar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+
+
+
+//ikikik
